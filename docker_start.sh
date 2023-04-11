0641cede839cd449a88d046316f868947994f4c6
Philip Huang <p208p2002@gmail.com>
2023-04-11 11:31:43 +0800

    update dockerfile

--- docker_start.sh
+++ docker_start.sh
@@ -2,7 +2,7 @@ if [ -f "already_ran" ]; then
     # pass
     echo "Already ran the Entrypoint once. Holding indefinitely for debugging.";
 else
-    echo dimage:$PASSWORD | sudo chpasswd;
+    echo $USERNAME:$PASSWORD | sudo chpasswd;
 fi
 
 sudo touch already_ran;
